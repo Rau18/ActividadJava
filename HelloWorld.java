ef64847e6f94afae433d733941caf3f3f89e72e3
Rau18 <101899502+Rau18@users.noreply.github.com>
2022-03-21 19:44:17 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hola Jenkins, mi nombre es Raúl."); 
+        System.out.println("Hola Jenkins, mi nombre es Raúl.")
     }
 }
 
